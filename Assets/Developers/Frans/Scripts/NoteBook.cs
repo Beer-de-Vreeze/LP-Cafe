84fcb8ed625bafbd7bac70552ed69a1f1be66e74
Frans Erhart <franserhart12@gmail.com>
2025-05-09 11:15:57 +0200

    Notebook??

---
+++ Assets/Developers/Frans/Scripts/NoteBook.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Notebook : MonoBehaviour
+{
+    private BachelorSO m_bachelorSO;
+
+    public void CheckBachelor(BachelorSO bachelor)
+    {
+        
+    }
+}
