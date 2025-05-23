6806e1f90f91d74849365830dbaf4876e5a48bcb
FemBear <beer@vreeze.com>
2025-05-23 09:29:09 +0200

    dialogue

---
+++ Assets/Developers/Beer/Scripts/NewBatchalor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBachelor", menuName = "Visual Novel/NewBachelor")]
+public class NewBachelorSO : ScriptableObject
+{
+    public string _name;
+    public string[] _knownLikes;
+    public string[] _knownDislikes;
+    public bool _isLikeDiscovered;
+    public bool _isDislikeDiscovered;
+}
