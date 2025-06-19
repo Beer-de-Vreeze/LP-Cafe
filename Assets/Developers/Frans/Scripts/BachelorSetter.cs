16f9a0588b43d1a31065209971e10b0d13fb2de1
FemBear <beer@vreeze.com>
2025-06-19 15:34:18 +0200

    Did it now it saves them after dating and opens the thing backup

--- Assets/Developers/Frans/Scripts/BachelorSetter.cs
+++ Assets/Developers/Frans/Scripts/BachelorSetter.cs
@@ -5,15 +5,30 @@ public class BachelorSetter : MonoBehaviour
 {
     [SerializeField]
     private NewBachelorSO m_bachelor;
+
     [SerializeField]
     private DialogueDisplay m_dialogueDisplay;
+
     [SerializeField]
     private Canvas m_canvas;
 
     void Start()
     {
         m_dialogueDisplay = FindFirstObjectByType<DialogueDisplay>();
-        m_canvas.enabled = false;
+        // Check if this bachelor has already been dated using the save system
+        var saveData = SaveSystem.Deserialize();
+        if (
+            saveData != null
+            && saveData.DatedBachelors != null
+            && saveData.DatedBachelors.Contains(m_bachelor.name)
+        )
+        {
+            DisableSetter();
+        }
+        else
+        {
+            m_canvas.enabled = false;
+        }
     }
 
     public void OnClick(BaseEventData data)
@@ -21,10 +36,10 @@ public class BachelorSetter : MonoBehaviour
         PointerEventData pData = (PointerEventData)data;
         Debug.Log(data);
         SetBatchelor();
-    }    
-    
-    
-    public void OnMouseDown(/*BaseEventData data*/)
+    }
+
+    public void OnMouseDown( /*BaseEventData data*/
+    )
     {
         /*        PointerEventData pData = (PointerEventData)data;
                 Debug.Log(data);*/
@@ -36,5 +51,21 @@ public class BachelorSetter : MonoBehaviour
     {
         m_canvas.enabled = true;
         m_dialogueDisplay.StartDialogue(m_bachelor, m_bachelor._dialogue);
+    }
+
+    public NewBachelorSO GetBachelor()
+    {
+        return m_bachelor;
+    }
+
+    public void DisableSetter()
+    {
+        if (m_canvas != null)
+            m_canvas.enabled = false;
+        // Optionally disable the whole GameObject or add more logic
+        var button = GetComponent<UnityEngine.UI.Button>();
+        if (button != null)
+            button.interactable = false;
+        this.enabled = false;
     }
 }
